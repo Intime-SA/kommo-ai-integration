dbb71cfdf087480fa3ccf22d4fcad81d81cfaaf5
RamAtlantic <arceramirom@gmail.com>
2025-10-28 01:34:36 -0300

    add cors

--- config/cors.js
+++ config/cors.js
@@ -2,22 +2,26 @@
 const corsConfig = {
   // URLs permitidas para CORS
   allowedOrigins: [
+    
     // kommo.paybot.app
     'https://kommo.paybot.app',
   
     // landings redirects token/campaigns
-    'https://guba-redirect-qhwv.vercel.app', // money maker
     'https://redirect-hernan-2.vercel.app',
     'https://redirect-tee-1.vercel.app',
+
+    // redirects kommo
     'https://guba-redirect.vercel.app', // guba redirect
-  'http://localhost:3000',  // Frontend típico
+    'https://guba-redirect-777.vercel.app', // guba redirect
+    'https://guba-redirect-888.vercel.app', // guba redirect
+/*   'http://localhost:3000',  // Frontend típico
     'http://127.0.0.1:3000', // Frontend alternativo
     'http://localhost:3001', // Si el backend está en 3001
     'http://127.0.0.1:3001',
     'http://localhost:3002', // Si el backend está en 3002
     'http://127.0.0.1:3002',
     'http://localhost:4000', // Otro puerto común
-    'http://localhost:8080', // Otro puerto común
+    'http://localhost:8080', // Otro puerto común */
   ],
 
   // Métodos HTTP permitidos
